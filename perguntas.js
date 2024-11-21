6a9a6ecf9b2591fd54d76bae6d97d67c9df47313
francielepaulino15 <140663792+francielepaulino15@users.noreply.github.com>
2024-11-20 21:13:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Historia',
+    'Quem foi o primeiro presidente dos Estados Unidos?',
+    'George Washington.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo?',
+    'Rússia.'
+)
+
+criaCartao(
+    'Matematica',
+    'O que é um número primo?',
+    'Um número que só pode ser dividido por 1 e por ele mesmo.'
+)
+
+criaCartao(
+    'Ciências',
+    'O que é o DNA?',
+    'Ácido desoxirribonucleico, responsável por armazenar a informação genética.'
+)
+criaCartao(
+    'Quimica',
+    'O que é uma reação química?',
+    'Transformação de substâncias em outras substâncias diferentes.'
+)
+criaCartao(
+    'Fisica',
+    'Qual é a unidade de medida da força no Sistema Internacional?',
+    'Newton (N).'
+)
